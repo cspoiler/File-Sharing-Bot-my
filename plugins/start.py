cf74d6f72f1597b98d8ed213493a02ba7d512c38
cspoiler <150610649+cspoiler@users.noreply.github.com>
2024-01-06 19:14:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -99,18 +99,6 @@ async def start_command(client: Client, message: Message):
                 
                 asyncio.create_task(delete_after_delay(k, AUTO_DELETE_TIME))
                 asyncio.create_task(delete_after_delay(h, AUTO_DELETE_TIME))
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                logger.warning(f"Floodwait of {e.x} sec.")
-                await client.send_cached_media(
-                    chat_id=message.from_user.id,
-                    file_id=msg.get("file_id"),
-                    caption=f_caption,
-                    protect_content=msg.get('protect', False),
-                    )
-            except Exception as e:
-                logger.warning(e, exc_info=True)
-                continue
             await asyncio.sleep(1) 
         await sts.delete()
         return
@@ -145,28 +133,16 @@ async def start_command(client: Client, message: Message):
             quote = True
         )
         try:
-                h = await message.reply_text(f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
-                k = await client.send_cached_media(
+                j = await message.reply_text(f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
+                p = await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
                 )
                 
-                asyncio.create_task(delete_after_delay(k, AUTO_DELETE_TIME))
-                asyncio.create_task(delete_after_delay(h, AUTO_DELETE_TIME))
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                logger.warning(f"Floodwait of {e.x} sec.")
-                await client.send_cached_media(
-                    chat_id=message.from_user.id,
-                    file_id=msg.get("file_id"),
-                    caption=f_caption,
-                    protect_content=msg.get('protect', False),
-                    )
-            except Exception as e:
-                logger.warning(e, exc_info=True)
-                continue
+                asyncio.create_task(delete_after_delay(j, AUTO_DELETE_TIME))
+                asyncio.create_task(delete_after_delay(p, AUTO_DELETE_TIME))
             await asyncio.sleep(1) 
         await sts.delete()
         return
